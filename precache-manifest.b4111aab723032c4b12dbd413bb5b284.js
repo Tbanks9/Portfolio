1a87eecab521a8eebff9815a93d1682742c4c4e7
Tbanks9 <timbanks9@hotmail.com>
2020-04-01 19:40:29 +0100

    Updates

---
+++ precache-manifest.b4111aab723032c4b12dbd413bb5b284.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273de463b4e6b5ebfe4067906a2ad203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c040aef9e69e31f8dc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0f9adcfdbcbe6927106d",
+    "url": "/static/css/main.7377a1cc.chunk.css"
+  },
+  {
+    "revision": "96c040aef9e69e31f8dc",
+    "url": "/static/js/2.85e123fa.chunk.js"
+  },
+  {
+    "revision": "c0a7484cc31358a0df86ce48ef08f02b",
+    "url": "/static/js/2.85e123fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9adcfdbcbe6927106d",
+    "url": "/static/js/main.a1db769d.chunk.js"
+  },
+  {
+    "revision": "94f45212ab4324ead759",
+    "url": "/static/js/runtime-main.78e3e998.js"
+  },
+  {
+    "revision": "2d9b3087348c8829ef95eb98e98c3c72",
+    "url": "/static/media/Chow-vow.2d9b3087.png"
+  },
+  {
+    "revision": "86058c9710fb151f0695d874d51e0335",
+    "url": "/static/media/Geography-genius-1.86058c97.png"
+  },
+  {
+    "revision": "202c95a152804a933a3b3bca4d2a90b1",
+    "url": "/static/media/Shotspot.202c95a1.png"
+  },
+  {
+    "revision": "607cf4817dfc04c3996b5d0d0f7724fc",
+    "url": "/static/media/Snake-1.607cf481.png"
+  }
+]);
